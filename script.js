a3d30f01a4313138202d48ee6b7afd6bdf881f35
norms2098 <nrmsdgdgn@gmail.com>
2023-03-08 14:08:41 +0800

    added initial factory functions needed

--- script.js
+++ script.js
@@ -2,12 +2,48 @@ const GameBoard = () =>{
     let boardArray = ["","","","","","","","",""];
 }
 
-const gameController = () =>{
-    const fields = document.querySelectorAll(".gamefield");
+//player things
+const Player = (symbol) =>{
+    let playerSymbol = symbol;
+    const getPlayerSymbol = () =>{
+        return playerSymbol;
+    }
+    return { getPlayerSymbol }
+}
 
-    fields.forEach((field)=> {
-        field.addEventListener('click',()=>{
-            
+//controls the flow of the game
+const gameFlowControl = () =>{
+    const playerO = Player("O");
+    const playerX = Player("X");
+    let roundCounter = 1 ;
+    let isGameOver = false;
+
+    const getCurrentPlayer = () => {
+        if (roundCounter % 2 == 0){
+            return playerO.getPlayerSymbol();
+        }
+        else{
+            return playerX.getPlayerSymbol();
+        }
+    }
+    const playRound = () =>{
+        
+    }
+    return { getCurrentPlayer }
+}
+
+//controls the game displays
+const gameDisplayControl = () => {
+    const gameFields = document.querySelectorAll('.gamefield');
+
+    gameFields.forEach((field)=>{
+        field.addEventListener('click', (e)=>{
+            if(e.target.textContent !== ""){
+                return;
+            }
+            console.log(e.dataset.indexNumber)
         })
     })
+
+
 }
