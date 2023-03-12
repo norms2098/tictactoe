af75eebd40fa093dc5c59a1b0a92e4fba1beb0b2
norms2098 <nrmsdgdgn@gmail.com>
2023-03-12 20:37:29 +0800

    added win conditions and can determine the winner

--- script.js
+++ script.js
@@ -19,12 +19,38 @@ const gameFlowControl = (() =>{
     const playerO = Player("O");
     const playerX = Player("X");
     let roundCounter = 0 ;
-    let isGameOver = false;
+    //let isGameOver = false;
+   
 
     const gameoverChecker = () =>{
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        winConditions.forEach((condition) => {
+            const [a,b,c] = condition ;
+            if ((GameBoard.boardArray[a] === GameBoard.boardArray[b] && GameBoard.boardArray[b] === GameBoard.boardArray[c])) {
+              if(GameBoard.boardArray[a]!== '' && GameBoard.boardArray[b]!=='' && GameBoard.boardArray[c]!==''){
+                if(roundCounter%2==0){
+                    gameDisplayControl.displayWinnerO();
+
+                }
+                else{
+                    gameDisplayControl.displayWinnerX();
+                }
+                return;
+              }
+            }
+          })
         roundCounter++
         if(roundCounter == 9) {
-            gameDisplayControl.displayGameOver();
+            gameDisplayControl.displayDraw();
         }
     }
 
@@ -41,15 +67,15 @@ const gameFlowControl = (() =>{
 
     const updateGameBoardArray = (index) =>{
         if (roundCounter %2 == 0){
-            GameBoard.boardArray[parseInt(index)-1]="O";
+            GameBoard.boardArray[parseInt(index)]="O";
         }
         else{
-            GameBoard.boardArray[parseInt(index)-1]="X";
+            GameBoard.boardArray[parseInt(index)]="X";
         }
         console.log(GameBoard.boardArray);
     }
     
-    return { getCurrentPlayer, roundCounter, gameoverChecker, isGameOver, updateGameBoardArray }
+    return { getCurrentPlayer, gameoverChecker, updateGameBoardArray }
 })();
 
 //controls the game displays
@@ -68,10 +94,20 @@ const gameDisplayControl = (() => {
         })
     })
 
-    const displayGameOver = () => {
+    const displayDraw = () => {
         console.log('gameover')
     }
+    const displayWinnerO = () =>{
+        alert("O wins!")
+        gameFields.forEach((field)=>{
+            field.setAttribute
+        })
+        
+    }
+    const displayWinnerX = () => {
+        alert("X wins!")
+    }
 
-    return { displayGameOver , }
+    return { displayDraw,displayWinnerO,displayWinnerX }
 })();
 
